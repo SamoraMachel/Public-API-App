7c363f80230afe7d4fdb4a4ebd2940c3aa54588b
Samora Machel <samora@work.laptop>
2022-07-06 12:27:48 +0300

    CREATE: create the entry and detail fragments

---
+++ app/src/main/java/com/googleio/publicapi/app/ui/EntryFragment.kt
@@ -0,0 +1,26 @@
+package com.googleio.publicapi.app.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.googleio.publicapi.R
+
+
+class EntryFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_entry, container, false)
+    }
+
+
+}
